14b487f2344b21bd2af6a822bc7a4e5e04e5c2ef
Nurettin Kaya <nurettinkaya@gmail.com>
2023-01-12 23:21:46 +0300

    2

--- src/A01.java
+++ src/A01.java
@@ -2,5 +2,7 @@ public class A01 {
     public static void main(String[] args) {
         System.out.println("Hello GitHub");
         System.out.println("Deneme");
+        System.out.println("deneme2");
+        System.out.println("....");
     }
 }
